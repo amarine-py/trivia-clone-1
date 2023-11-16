3f5324425cf8078bbf8617eec4ccb223dc6f9d1d
Alex Marine <amarine@gmail.com>
2023-11-16 17:27:45 -0500

    set up react project, installed some modules, and created a basic navbar

---
+++ front-end/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <header>
+      <img id="logo" src=""></img>
+      <h1>Jeopardy-Style Trivia Clone</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="">
+              New Game
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
